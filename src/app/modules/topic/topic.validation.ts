9bdd87a8c3964a6d60f127539de206e7c92d3747
manik <maniksarker652@gmail.com>
2025-06-03 14:41:31 +0600

    make some changes

--- src/app/modules/topic/topic.validation.ts
+++ src/app/modules/topic/topic.validation.ts
@@ -1,13 +1,7 @@
 import { z } from 'zod';
-import { Types } from 'mongoose';
 export const createTopicValidationSchema = z.object({
     body: z.object({
         name: z.string({ required_error: 'Name is required' }),
-        category: z
-            .string({ required_error: 'Category is required' })
-            .refine((value) => Types.ObjectId.isValid(value), {
-                message: 'Invalid ObjectId format for category',
-            }),
     }),
 });
 
