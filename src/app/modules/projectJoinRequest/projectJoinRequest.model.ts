f7dbbb5e94ec94ac9aaf16b64d973b9e6fff265a
manik <maniksarker652@gmail.com>
2025-06-15 09:38:58 +0600

    create project join request model

--- src/app/modules/projectJoinRequest/projectJoinRequest.model.ts
+++ src/app/modules/projectJoinRequest/projectJoinRequest.model.ts
@@ -1,16 +1,31 @@
-import { model, Schema } from "mongoose";
-import { IProjectJoinRequest } from "./projectJoinRequest.interface";
+import { Schema, model } from 'mongoose';
+import { IProjectJoinRequest } from './projectJoinRequest.interface';
+import { ENUM_PROJECT_JOIN_REQEST_STATUS } from './projectJoinRequest.enum';
 
-const projectJoinRequestSchema = new Schema<IProjectJoinRequest>({
-    user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-    name: { type: String, required: true },
-    phone: { type: String },
-    email: { type: String, required: true, unique: true },
-    address: { type: String },
-    profile_image: { type: String, default: "" },
-    totalAmount: { type: Number, default: 0 },
-    totalPoint: { type: Number, default: 0 }
-}, { timestamps: true });
+const projectJoinRequestSchema = new Schema<IProjectJoinRequest>(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'NormalUser',
+            required: true,
+        },
+        project: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: Object.values(ENUM_PROJECT_JOIN_REQEST_STATUS),
+            default: ENUM_PROJECT_JOIN_REQEST_STATUS.Pending,
+        },
+    },
+    { timestamps: true }
+);
 
-const projectJoinRequestModel = model<IProjectJoinRequest>("ProjectJoinRequest", projectJoinRequestSchema);
-export default projectJoinRequestModel;
+const ProjectJoinRequestModel = model<IProjectJoinRequest>(
+    'ProjectJoinRequest',
+    projectJoinRequestSchema
+);
+
+export default ProjectJoinRequestModel;
