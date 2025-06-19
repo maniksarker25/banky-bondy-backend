09abba0541c0afcf243eb6cc68dd098fcaa0c1bb
manik <maniksarker652@gmail.com>
2025-06-19 16:51:58 +0600

    create service and contgroller for friend request

--- src/app/modules/friendRequest/friendRequest.controller.ts
+++ src/app/modules/friendRequest/friendRequest.controller.ts
@@ -1,24 +1,98 @@
-import httpStatus from "http-status";
-import catchAsync from "../../utilities/catchasync";
-import sendResponse from "../../utilities/sendResponse";
-import friendRequestServices from "./friendRequest.service";
+import httpStatus from 'http-status';
+import catchAsync from '../../utilities/catchasync';
+import sendResponse from '../../utilities/sendResponse';
+import friendRequestService from './friendRequest.service';
 
-const updateUserProfile = catchAsync(async (req, res) => {
-    const { files } = req;
-    if (files && typeof files === "object" && "profile_image" in files) {
-        req.body.profile_image = files["profile_image"][0].path;
-    }
-    const result = await friendRequestServices.updateUserProfile(
-        req.user.profileId,
-        req.body
+const sendFriendRequest = catchAsync(async (req, res) => {
+    const result = await friendRequestService.sendFriendRequest(
+        req.user._id,
+        req.body.receiver
     );
     sendResponse(res, {
-        statusCode: httpStatus.OK,
+        statusCode: httpStatus.CREATED,
         success: true,
-        message: "Profile updated successfully",
+        message: 'Friend request sent',
         data: result,
     });
 });
 
-const FriendRequestController = { updateUserProfile };
-export default FriendRequestController;
+const updateRequestStatus = catchAsync(async (req, res) => {
+    const result = await friendRequestService.updateFriendRequestStatus(
+        req.params.id,
+        req.body.status
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Friend request status updated',
+        data: result,
+    });
+});
+
+const getFriends = catchAsync(async (req, res) => {
+    const result = await friendRequestService.getMyFriends(req.user._id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Friends retrieved',
+        data: result,
+    });
+});
+
+const getFollowers = catchAsync(async (req, res) => {
+    const result = await friendRequestService.getMyFollowers(req.user._id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Followers retrieved',
+        data: result,
+    });
+});
+
+const getFollowing = catchAsync(async (req, res) => {
+    const result = await friendRequestService.getMyFollowing(req.user._id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Following retrieved',
+        data: result,
+    });
+});
+
+const cancelRequest = catchAsync(async (req, res) => {
+    const result = await friendRequestService.cancelSentRequest(
+        req.user._id,
+        req.params.receiverId
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Request canceled',
+        data: result,
+    });
+});
+
+const unfriend = catchAsync(async (req, res) => {
+    const result = await friendRequestService.unfriend(
+        req.user._id,
+        req.params.friendId
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Unfriended successfully',
+        data: result,
+    });
+});
+
+const friendRequestController = {
+    sendFriendRequest,
+    updateRequestStatus,
+    getFriends,
+    getFollowers,
+    getFollowing,
+    cancelRequest,
+    unfriend,
+};
+
+export default friendRequestController;
