b1a890dc4e3b9b3ceefa820263e96d9f1fabe1eb
manik <maniksarker652@gmail.com>
2025-06-19 16:52:19 +0600

    route setup for friend reqeust

--- src/app/modules/friendRequest/friendRequest.routes.ts
+++ src/app/modules/friendRequest/friendRequest.routes.ts
@@ -1,25 +1,53 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import validateRequest from "../../middlewares/validateRequest";
-import friendRequestValidations from "./friendRequest.validation";
-import friendRequestController from "./friendRequest.controller";
-import { uploadFile } from "../../helper/fileUploader";
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import validateRequest from '../../middlewares/validateRequest';
+import friendRequestValidation from './friendRequest.validation';
+import friendRequestController from './friendRequest.controller';
 
 const router = express.Router();
 
-router.patch(
-    "/update-profile",
+router.post(
+    '/send',
     auth(USER_ROLE.user),
-    uploadFile(),
-    (req, res, next) => {
-        if (req.body.data) {
-            req.body = JSON.parse(req.body.data);
-        }
-        next();
-    },
-    validateRequest(friendRequestValidations.updateFriendRequestData),
-    friendRequestController.updateUserProfile
+    validateRequest(friendRequestValidation.createFriendRequestValidation),
+    friendRequestController.sendFriendRequest
+);
+
+router.patch(
+    '/update-status/:id',
+    auth(USER_ROLE.user),
+    validateRequest(
+        friendRequestValidation.updateFriendRequestStatusValidation
+    ),
+    friendRequestController.updateRequestStatus
+);
+
+router.get(
+    '/friends',
+    auth(USER_ROLE.user),
+    friendRequestController.getFriends
+);
+router.get(
+    '/followers',
+    auth(USER_ROLE.user),
+    friendRequestController.getFollowers
+);
+router.get(
+    '/following',
+    auth(USER_ROLE.user),
+    friendRequestController.getFollowing
+);
+
+router.delete(
+    '/cancel/:receiverId',
+    auth(USER_ROLE.user),
+    friendRequestController.cancelRequest
+);
+router.delete(
+    '/unfriend/:friendId',
+    auth(USER_ROLE.user),
+    friendRequestController.unfriend
 );
 
 export const friendRequestRoutes = router;
