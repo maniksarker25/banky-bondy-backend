98172b4ead1f84887394ab653512e0988f331079
manik <maniksarker652@gmail.com>
2025-06-21 14:09:45 +0600

    create validation schema for bond link model

--- src/app/modules/bondLink/bondLink.validation.ts
+++ src/app/modules/bondLink/bondLink.validation.ts
@@ -1,12 +1,23 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-export const updateBondLinkData = z.object({
+import { ENUM_BOND_LINK_STATUS } from './bondLink.enum';
+
+export const createBondLinkSchema = z.object({
     body: z.object({
-        name: z.string().optional(),
-        phone: z.string().optional(),
-        address: z.string().optional(),
+        name: z.string({
+            required_error: 'Name is required',
+        }),
+        participants: z
+            .array(z.string().regex(/^[a-f\d]{24}$/i, 'Invalid ObjectId'))
+            .optional(),
+        requestedBonds: z
+            .array(z.string().regex(/^[a-f\d]{24}$/i, 'Invalid ObjectId'))
+            .optional(),
+        status: z
+            .enum(Object.values(ENUM_BOND_LINK_STATUS) as [string, ...string[]])
+            .optional(),
     }),
 });
 
-const BondLinkValidations = { updateBondLinkData };
+const BondLinkValidations = { createBondLinkSchema };
 export default BondLinkValidations;
