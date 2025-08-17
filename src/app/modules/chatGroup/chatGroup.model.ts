19bc10f1006bb0305b460a6cf14eb7e58c3f2b5f
manik <maniksarker652@gmail.com>
2025-08-17 09:56:06 +0600

    create model for chat group

--- src/app/modules/chatGroup/chatGroup.model.ts
+++ src/app/modules/chatGroup/chatGroup.model.ts
@@ -1,16 +1,19 @@
-import { model, Schema } from "mongoose";
-import { IChatGroup } from "./chatGroup.interface";
+import { model, Schema } from 'mongoose';
+import { IChatGroup } from './chatGroup.interface';
 
-const chatGroupSchema = new Schema<IChatGroup>({
-    user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-    name: { type: String, required: true },
-    phone: { type: String },
-    email: { type: String, required: true, unique: true },
-    address: { type: String },
-    profile_image: { type: String, default: "" },
-    totalAmount: { type: Number, default: 0 },
-    totalPoint: { type: Number, default: 0 }
-}, { timestamps: true });
+const chatGroupSchema = new Schema<IChatGroup>(
+    {
+        name: {
+            type: String,
+        },
+        participants: {
+            type: [Schema.Types.ObjectId],
+            ref: 'NormalUser',
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
 
-const chatGroupModel = model<IChatGroup>("ChatGroup", chatGroupSchema);
+const chatGroupModel = model<IChatGroup>('ChatGroup', chatGroupSchema);
 export default chatGroupModel;
