6eb2c5a1975dbb84baea3cb827c1aa1ed30a1df6
manik <maniksarker652@gmail.com>
2025-09-13 16:42:27 +0600

    create bond rating controller

--- src/app/modules/bondRating/bondRating.controller.ts
+++ src/app/modules/bondRating/bondRating.controller.ts
@@ -1,24 +1,20 @@
-import httpStatus from "http-status";
-import catchAsync from "../../utilities/catchasync";
-import sendResponse from "../../utilities/sendResponse";
-import bondRatingServices from "./bondRating.service";
+import httpStatus from 'http-status';
+import catchAsync from '../../utilities/catchasync';
+import sendResponse from '../../utilities/sendResponse';
+import bondRatingServices from './bondRating.service';
 
-const updateUserProfile = catchAsync(async (req, res) => {
-    const { files } = req;
-    if (files && typeof files === "object" && "profile_image" in files) {
-        req.body.profile_image = files["profile_image"][0].path;
-    }
-    const result = await bondRatingServices.updateUserProfile(
+const addRating = catchAsync(async (req, res) => {
+    const result = await bondRatingServices.addRating(
         req.user.profileId,
         req.body
     );
     sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
-        message: "Profile updated successfully",
+        message: 'Rating added successfully',
         data: result,
     });
 });
 
-const BondRatingController = { updateUserProfile };
+const BondRatingController = { addRating };
 export default BondRatingController;
